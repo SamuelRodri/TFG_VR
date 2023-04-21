944f6a87de0cda58d45a39e69c5723bf61efdd1f
SamuelRodri <samuelrh2001@hotmail.com>
2023-04-21 11:23:48 +0100

    Jerarquía de huesos arreglada con editor scripting

---
+++ Assets/Scripts/Editor/BreakHierarchyEditor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace TFG.Utils
+{
+    [CustomEditor(typeof(BreakHierarchy))]
+    [CanEditMultipleObjects]
+    public class BreakHierarchyEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            BreakHierarchy myScript = (BreakHierarchy)target;
+
+            if(GUILayout.Button("Set Parent"))
+            {
+                myScript.SetParent();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/BreakHierarchy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TFG.Utils
+{
+    // Set a parent object breking the hierarchy
+    public class BreakHierarchy : MonoBehaviour
+    {
+        public GameObject parent;
+
+        public void SetParent()
+        {
+            if(transform.childCount > 0)
+            {
+                try
+                {
+                    transform.parent = parent.transform;
+                    transform.GetChild(0).GetComponent<BreakHierarchy>().SetParent();
+                }
+                catch
+                {
+                    Debug.Log("No lo tiene");
+                }
+            }
+        }
+    }
+}
