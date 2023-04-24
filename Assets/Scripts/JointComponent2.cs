a3dc02a6aeb5801f12e157e8462d8e5786f3f6ba
SamuelRodri <samuelrh2001@hotmail.com>
2023-04-24 15:05:20 +0100

    Previo a probar SpaceWarp

--- Assets/Scripts/JointComponent2.cs
+++ Assets/Scripts/JointComponent2.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class JointComponent2 : MonoBehaviour
 {
     public GameObject prevObject;
+    public GameObject nextObject;
+
     private Rigidbody rb;
     private Vector3 linearoffset;
     private Quaternion rotationalOffset;
     private Quaternion rotationInicial;
 
-    public bool isGrabbed = false;
     public float umbral = 0.006f;
 
     private Vector3 antPrev;
@@ -20,11 +21,19 @@ public class JointComponent2 : MonoBehaviour
     Matrix4x4 translationMatrix;
     Matrix4x4 rotationMatrix;
 
-    bool isDone = false;
+    public bool isGrabbed = false;
 
-    private Vector3 relativePosition;
-    private Quaternion relativeRotation;
+    private Vector3 relativePositionNext;
+    private Quaternion relativeRotationNext;
+    private Vector3 relativePositionPrev;
+    private Quaternion relativeRotationPrev;
     private Vector3 relativeScale;
+
+    private Vector3 finalPosNext;
+    private Vector3 finalPosPrev;
+
+    private Quaternion finalRotNext;
+    private Quaternion finalRotPrev;
 
     private void Awake()
     {
@@ -33,69 +42,83 @@ public class JointComponent2 : MonoBehaviour
 
     private void Start()
     {
-        linearoffset = transform.position - prevObject.transform.position;
-        rotationalOffset = Quaternion.Inverse(prevObject.transform.rotation) * transform.rotation;
-        Debug.Log(rotationalOffset.eulerAngles);
-        translationMatrix = Matrix4x4.Translate(linearoffset);
-        rotationMatrix = Matrix4x4.Rotate(rotationalOffset);
+        finalPosPrev = transform.position;
+        finalPosNext = transform.position;
 
-        relativePosition = prevObject.transform.InverseTransformPoint(transform.position);
-        relativeRotation = Quaternion.Inverse(prevObject.transform.rotation) * transform.rotation;
-        //relativeScale = Vector3.Scale(transform.localScale, Vector3.one / prevObject.transform.localScale.x);
+        if (nextObject)
+        {
+            relativePositionNext = nextObject.transform.InverseTransformPoint(transform.position);
+            relativeRotationNext = Quaternion.Inverse(nextObject.transform.rotation) * transform.rotation;
+        }
+
+        if (prevObject)
+        {
+            relativePositionPrev = prevObject.transform.InverseTransformPoint(transform.position);
+            relativeRotationPrev = Quaternion.Inverse(prevObject.transform.rotation) * transform.rotation;
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (isDone) return;
-        //Matrix4x4 a1 = prevObject.transform.localToWorldMatrix;
-        //a1 = Matrix4x4.TRS(a1.GetColumn(3), Quaternion.LookRotation(a1.GetColumn(2), a1.GetColumn(1)), Vector3.one);
-        //Debug.Log($"a1: {a1.ToString("f2")}");
-
-        //Matrix4x4 m0 = transform.localToWorldMatrix;
-        //m0 = Matrix4x4.TRS(m0.GetColumn(3), Quaternion.LookRotation(m0.GetColumn(2), m0.GetColumn(1)), Vector3.one);
-        //Debug.Log($"m0: {m0.ToString("f2")}");
-
-        //Matrix4x4 t = a1 * Matrix4x4.Inverse(m0);
-        ////Debug.Log($"t: {t.ToString("f1")}");
-        //Matrix4x4 b = t * m0;
-
-        //Matrix4x4 final = b * translationMatrix;
-        //transform.position = final.GetColumn(3);
-        //final *= rotationMatrix;
-        ////transform.rotation = Quaternion.LookRotation(final.GetColumn(2), final.GetColumn(1));
-        //transform.position = prevObject.transform.position + linearoffset;
-        //var rotacionPadreGlobal = prevObject.transform.rotation;
-        //var rotacionLocal = Quaternion.Inverse(prevObject.transform.rotation) * transform.localRotation;
-        //transform.rotation = rotacionPadreGlobal * rotacionLocal;
-        isDone = true;
-
-        //Matrix4x4 a1 = prevObject.transform.localToWorldMatrix;
-        //a1 = Matrix4x4.TRS(a1.GetColumn(3), Quaternion.LookRotation(a1.GetColumn(2), a1.GetColumn(1)), Vector3.one);
-
-        //Matrix4x4 m0 = transform.localToWorldMatrix;
-        //m0 = Matrix4x4.TRS(m0.GetColumn(3), Quaternion.LookRotation(m0.GetColumn(2), m0.GetColumn(1)), Vector3.one);
-
-        //Matrix4x4 transformMatrix = translationMatrix * Matrix4x4.Inverse(rotationMatrix);
-
-        //Matrix4x4 final = a1 * translationMatrix * rotationMatrix;
-        //transform.position = final.GetColumn(3);
-        //transform.rotation = Quaternion.LookRotation(final.GetColumn(2), final.GetColumn(1));
-
-        if(Vector3.Distance(prevObject.transform.TransformPoint(relativePosition), transform.position) > 0.002f)
+        if (nextObject) // Tiene "hijo"
         {
-            Vector3 target = prevObject.transform.TransformPoint(relativePosition);
-            Vector3 newPosition = Vector3.Lerp(transform.position, target, 0.57f);
+                Vector3 target = nextObject.transform.TransformPoint(relativePositionNext);
+                Vector3 newPosition = Vector3.Lerp(transform.position, target, 50 * Time.deltaTime);
 
-            transform.position = newPosition;
+                finalPosNext = newPosition;
+
+                Quaternion targetRot = nextObject.transform.rotation * relativeRotationNext;
+                Quaternion newRotation = Quaternion.Slerp(transform.rotation, targetRot, 50 * Time.deltaTime);
+
+                transform.rotation = newRotation;
         }
 
-        if(Quaternion.Angle(prevObject.transform.rotation * relativeRotation, transform.rotation) > 2.5f)
+        if (prevObject) // Tiene "padre"
         {
-            Quaternion target = prevObject.transform.rotation * relativeRotation;
-            Quaternion newRotation = Quaternion.Slerp(transform.rotation, target, 0.45f);
+                Vector3 target = prevObject.transform.TransformPoint(relativePositionPrev);
+                Vector3 newPosition = Vector3.Lerp(transform.position, target, 50 * Time.deltaTime);
 
-            transform.rotation = newRotation;
+                finalPosPrev = newPosition;
+
+                Quaternion targetRot = prevObject.transform.rotation * relativeRotationPrev;
+                Quaternion newRotation = Quaternion.Slerp(transform.rotation, targetRot, 50 * Time.deltaTime);
+
+                transform.rotation = newRotation;
+        }
+
+        if (!isGrabbed)
+        {
+            if (prevObject) // Si tiene padre
+            {
+                if (nextObject) // Si tiene hijo
+                {
+                    if (!prevObject.GetComponent<JointComponent2>().isGrabbed && nextObject.GetComponent<JointComponent2>().isGrabbed)
+                    {
+                        transform.position = Vector3.Slerp(finalPosPrev, finalPosNext, 0.8f);
+                    }else if (prevObject.GetComponent<JointComponent2>().isGrabbed && !nextObject.GetComponent<JointComponent2>().isGrabbed)
+                    {
+                        transform.position = Vector3.Slerp(finalPosPrev, finalPosNext, 0.2f);
+                    }
+                    else
+                    {
+                        transform.position = Vector3.Slerp(finalPosPrev, finalPosNext, 0.5f);
+                    }
+                }
+                else
+                {
+                    transform.position = finalPosPrev;
+                }
+            }
+            else // Si no tiene padre
+            {
+                if (nextObject) // Si tiene hijo
+                {
+                    transform.position = finalPosNext;
+                }
+            }
+            
         }
     }
 }
