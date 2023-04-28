e6bcea48c607e3fa56692f3ec43aca35d9ee6e44
SamuelRodri <samuelrh2001@hotmail.com>
2023-04-28 10:17:48 +0100

    Vertebras se siguen sin delay al 'agarrar' una

--- Assets/Scripts/JointComponentGraph.cs
+++ Assets/Scripts/JointComponentGraph.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class JointComponentGraph : MonoBehaviour
 {
+    public bool isGrabbed;
+
+    [SerializeField] GameObject prevObject;
+    [SerializeField] GameObject nextObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,32 @@ public class JointComponentGraph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGrabbed) // Está siendo agarrado
+        {
+            if (prevObject)
+            {
+                prevObject.GetComponent<JointComponentGraph>().FollowObject(gameObject);
+            }
+
+            if (nextObject)
+            {
+                nextObject.GetComponent<JointComponentGraph>().FollowObject(gameObject);
+            }
+        }
+    }
+
+    public void FollowObject(GameObject obj)
+    {
+        transform.position = obj.transform.position;
+
+        if(obj.Equals(prevObject) && nextObject)
+        {
+            nextObject.GetComponent<JointComponentGraph>().FollowObject(gameObject);
+        }
+
+        if(obj.Equals(nextObject) && prevObject)
+        {
+            prevObject.GetComponent<JointComponentGraph>().FollowObject(gameObject);
+        }
     }
 }
