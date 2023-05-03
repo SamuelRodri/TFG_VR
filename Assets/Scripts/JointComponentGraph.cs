e13f5df1fece661d11be8640072858deaf734791
SamuelRodri <samuelrh2001@hotmail.com>
2023-05-03 09:13:07 +0100

    SoftLimit lineal

--- Assets/Scripts/JointComponentGraph.cs
+++ Assets/Scripts/JointComponentGraph.cs
@@ -22,6 +22,8 @@ public class JointComponentGraph : MonoBehaviour
 
     private bool isMoved;
 
+    private float softLimitPosition = 0.001f;
+
     private void Start()
     {
         if (prevObject)
@@ -55,6 +57,7 @@ public class JointComponentGraph : MonoBehaviour
 
             if (nextObject) // Si tiene siguiente hacemos que nos siga
             {
+                Debug.Log("Llama a next");
                 nextObject.UpdateTransformToFollowObject(this);
             }
 
@@ -71,19 +74,34 @@ public class JointComponentGraph : MonoBehaviour
     public void UpdateTransformToFollowObject(JointComponentGraph objectToFollow)
     {
         if (isMoved || isGrabbed) return;
-
-        float prevDistance = (prevObject) ? Vector3.Distance(transform.position, prevObject.transform.position) : 0;
-        float nextDistance = (nextObject) ? Vector3.Distance(transform.position, nextObject.transform.position) : 0;
         
         (Vector3 relativePos, Quaternion relativeRot) = GetRelativeTransform(objectToFollow);
 
         Vector3 targetPosition = objectToFollow.transform.TransformPoint(relativePos);
         Quaternion targetRotation = objectToFollow.transform.rotation * relativeRot;
 
-        transform.position = targetPosition;
+        Debug.Log($"Mi prev está en la posicion: {prevObject.transform.position}");
+        Debug.Log($"Y mi target position en: {targetPosition}");
+        // Soft Limit
+        float prevDistance = (prevObject) ? Vector3.Distance(transform.position, prevObject.transform.position) : 0;
+        float nextDistance = (nextObject) ? Vector3.Distance(transform.position, nextObject.transform.position) : 0;
+
+        float prevDistanceDiff = Math.Abs(prevDistance - firstPrevDistance);
+        float nextDistanceDiff = Math.Abs(nextDistance - firstNextDistance);
+
+        Debug.Log(prevDistance);
+
+        if (prevDistanceDiff > softLimitPosition || nextDistanceDiff > softLimitPosition)
+        {
+            transform.position = targetPosition;
+
+            isMoved = true;
+
+        }
+
         transform.rotation = targetRotation;
 
-        isMoved = true;
+        
         JointComponentGraph neighbor = (nextObject && objectToFollow != nextObject) ? nextObject :
                                         (prevObject && objectToFollow != prevObject) ? prevObject : null;
 
