9e78fa1107a4c17a7752cbf42bd48dfaf6ad8aa4
SamuelRodri <samuelrh2001@hotmail.com>
2023-05-05 10:39:02 +0100

    Carteles re-implementados en discos y vertebras

--- Assets/Scripts/XROffsetGrabInteractable.cs
+++ Assets/Scripts/XROffsetGrabInteractable.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TFG.Behaviour;
+using TMPro;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -19,10 +21,16 @@ public class XROffsetGrabInteractable : XRGrabInteractable
 
     public Vector3 rotation;
 
+    [SerializeField] GameObject cartel;
+    [SerializeField] TMP_Text information;
 
     // Start is called before the first frame update
     void Start()
     {
+        cartel = transform.GetChild(0).gameObject;
+        information = cartel.transform.GetChild(0).transform.GetChild(0).GetComponent<TMP_Text>();
+        information.text = name;
+
         // Create attach point
         if (!attachTransform)
         {
@@ -53,11 +61,18 @@ public class XROffsetGrabInteractable : XRGrabInteractable
 
         offset = Quaternion.Inverse(interactor.transform.rotation) * transform.rotation;
         positionOffset = transform.position - interactor.transform.position;
+
+        if (!FindObjectOfType<SimulationController>().areJointsActivated)
+        {
+            cartel.SetActive(true);
+        }
+
         base.OnSelectEntering(args);
     }
 
     protected override void OnSelectExited(SelectExitEventArgs args)
     {
+        cartel.SetActive(false);
         isGrabbed = false;
         base.OnSelectExited(args);
     }
